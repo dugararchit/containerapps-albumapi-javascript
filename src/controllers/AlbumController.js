853243a7f735a1b6d7dfad2f6d28c7f8fcc73b5d
dugararchit <dugararchit@gmail.com>
2024-08-10 12:14:38 +0530

    Update AlbumController.js

--- src/controllers/AlbumController.js
+++ src/controllers/AlbumController.js
@@ -2,6 +2,6 @@ var Albums = require("../models/Album");
 
 exports.index = async function (req, res) {
   const albums = await Albums.getAlbums();
-  console.log("Retrieved Albums");
+  console.log("Retrieved Albums2");
   res.json(albums);
 };
